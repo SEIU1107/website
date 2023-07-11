3df2202e323894dc4e42c5c0164f3092f79b0339
Nathan Mactal <ntmactal@gmail.com>
2023-07-10 17:15:59 -0700

    Continue tailwind migration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'Roboto': 'Roboto, sans-serif',
+				'Verdana': 'Verdana, serif',
+				'Trebuchet': '"Trebuchet MS", serif'
+			},
+			colors: {
+				'purple': {
+					primary: '#582C83',
+					secondary: '#8d30ee',
+					darker: '#371E51',
+					darkest: '#251338',
+				},
+				'yellow': '#FECD01',
+				'lavender': '#EADEF7',
+				'white': '#ffffff',
+				'black': '#000000'
+			}
+		},
 	},
 	plugins: [],
 }
