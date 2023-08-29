b4a3a759ccf8736f1ec27002007938f7939abef5
Nathan Mactal <ntmactal@gmail.com>
2023-08-28 17:10:20 -0700

    Refactored info cards

---
+++ src/components/ImageCarousel.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let images;
+    import Carousel from 'svelte-carousel';
+</script>
+
+<Carousel
+    autoplay
+    autoplayDuration={5000}
+    autoplayProgressVisible
+    pauseOnFocus>
+    {#each images as img}
+    <img class="rounded-md object-cover" src={img} alt="">   
+    {/each}
+</Carousel>
