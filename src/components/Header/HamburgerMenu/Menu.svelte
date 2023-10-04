a3819b32db953467edcbf4f0a752fad66319a2be
Nathan Mactal <ntmactal@gmail.com>
2023-10-04 17:04:32 +0800

    HAMBURGER MENU FINISHED LETS GOOOO

---
+++ src/components/Header/HamburgerMenu/Menu.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import { fly } from 'svelte/transition'
+  
+    import AnimatedHamburger from "./AnimatedHamburger.svelte"
+
+    export let open = false
+    export let onClick = (): void => {
+      open = !open
+    }
+
+    import { all_dropdowns } from "../NavData.ts";
+
+    let activeMenu = 'main';
+  </script>
+  
+  <header>
+    <div class="main">
+      <AnimatedHamburger {open} {onClick} />
+    </div>
+  
+    {#if open}
+
+        <div class="max-w-screen-xl bg-gray-200 p-5 m-5 rounded-md shadow-sm shadow-black flex-col" transition:fly={{ y: -200, duration: 400 }}>
+
+
+            {#if activeMenu === 'main'}
+            <div class="text-2xl" in:fly={{ x: -300 }} out:fly={{ x: -300 }}>
+                {#each all_dropdowns as dropdown}
+                {#if dropdown.content}
+                <button on:click={() => activeMenu = dropdown.title} transition:fly={{ y: -200, duration: 400 }} class="h-12 grid grid-cols-1 gap-3 align-middle rounded-md transition-colors ease-in-out duration-300 p-2 text-purple-700 active:bg-gray-300 hover:underline active:no-underline">
+                    {dropdown.title}
+                </button>
+                {:else}
+                <button 
+                on:click={() => activeMenu = 'main'} 
+                class="h-12 flex flex-col py-2 my-2 align-middle rounded-md transition-colors ease-in-out duration-300 p-2 text-white bg-purple-950 active:bg-purple-800 hover:underline active:no-underline">
+                    <a href={dropdown.titleHref}>{dropdown.title}</a>
+                </button>
+                {/if}
+                {/each}    
+            </div>
+            {/if}
+            
+            {#each all_dropdowns as dropdown}
+            {#if activeMenu === dropdown.title}
+            <div class="text-lg" in:fly={{ x: 300 }} out:fly={{ x: 300 }}>
+                <button 
+                on:click={() => activeMenu = 'main'} 
+                class="h-12 flex flex-col py-2 my-1 text-2xl font-bold align-middle rounded-md transition-colors ease-in-out duration-300 p-2 text-black active:bg-gray-300 hover:underline active:no-underline">
+                    Back
+                </button>
+                <button 
+                on:click={() => activeMenu = 'main'} 
+                class="h-12 flex flex-col py-2 my-2 align-middle rounded-md transition-colors ease-in-out duration-300 p-2 text-white bg-purple-950 active:bg-purple-800 hover:underline active:no-underline">
+                    <a href={dropdown.titleHref}>{dropdown.title}</a>
+                </button>
+                {#if dropdown.content}
+                {#each dropdown.content as content}
+                <button 
+                on:click={() => activeMenu = 'main'} 
+                class="h-12 flex flex-col py-2 my-2 align-middle rounded-md transition-colors ease-in-out duration-300 p-2 text-white bg-purple-950 active:bg-purple-800 hover:underline active:no-underline">
+                    <a href={content.href}>{content.pageName}</a>
+                </button>
+                {/each}
+                {/if}
+                    
+                    
+            </div>
+            
+            {/if}
+            {/each}
+            
+        </div>
+
+    {/if}
+  </header>
+  
+  <style>
+    header {
+      position: relative;
+      font-size: 2rem;
+      color: var(--accent-color);
+      background-color: var(--white);
+      z-index: 2;
+    }
+  
+    .main {
+      display: flex;
+      justify-content: space-between;
+    }
+
+  </style>
---
+++ src/components/Header/NavData.ts
@@ -0,0 +1,108 @@
+/* About Us Dropdown */
+const dropdownAboutUs = [
+    {
+        pageName: "Why a Union?",
+        href: "/union_difference/",
+    },
+    {
+        pageName: "Executive Board",
+        href: "/executive_board",
+    },
+    {
+        pageName: "Staff",
+        href: "/staff_page",
+    },
+    {
+        pageName: "Contact Us",
+        href: "/contact_us/",
+    },
+    {
+        pageName: "Work with Us",
+        href: "/careers/",
+    },
+];
+
+/* My Union */
+const myUnion = [
+    {
+        pageName: "My Stewards",
+        href: "/stewards",
+    },
+    {
+        pageName: "My Member Benefits",
+        href: "https://www.seiumb.com/",
+    },
+    {
+        pageName: "Constitution & Bylaws",
+        href: "/constitution",
+    },
+    {
+        pageName: "Committees & Caucus",
+        href: "/",
+    },
+    {
+        pageName: "Training & Education",
+        href: "/",
+    },
+];
+
+const getInvolved = [
+    {
+        pageName: "Events Calendar",
+        href: "/events_calendar",
+    },
+    {
+        pageName: "Know Your Rights",
+        href: "/",
+    },
+    {
+        pageName: "Power of Politics",
+        href: "/",
+    },
+    {
+        pageName: "Communities United",
+        href: "/",
+    },
+    {
+        pageName: "Home Care Workers United",
+        href: "/",
+    },
+];
+
+const updatesPages = [
+    {
+        pageName: "Press Releases",
+        href: "/",
+    },
+    {
+        pageName: "In The News",
+        href: "/",
+    },
+];
+
+export const all_dropdowns = [
+    {
+        title: "About Us",
+        titleHref: "/about_us",
+        content: dropdownAboutUs,
+    },
+    {
+        title: "My Union",
+        titleHref: "/",
+        content: myUnion,
+    },
+    {
+        title: "Get Involved",
+        titleHref: "/",
+        content: getInvolved,
+    },
+    {
+        title: "1107 Updates",
+        titleHref: "/news_updates",
+        content: updatesPages,
+    },
+    {
+        title: "Join Now!",
+        titleHref: "/join",
+    },
+];
