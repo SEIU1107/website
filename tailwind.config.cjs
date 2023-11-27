0f5290050d84deaddbd707f4d2a7a8bc2c1f67ef
Nathan Mactal <ntmactal@gmail.com>
2023-11-26 17:10:28 -0800

    change banner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				'Roboto': 'Roboto, sans-serif',
-				'Trebuchet': '"Trebuchet MS", serif',
+				'Trebuchet': '"Trebuchet MS", sans-serif',
+				'Eras': ''
 			},
 			colors: {
 				'honey-flower': {
