0187436be56f9472adb4bb8433f3a0d745ea00c3
Nathan Mactal <ntmactal@gmail.com>
2023-12-08 11:44:29 -0800

    added deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rezcom.github.io',
   integrations: [tailwind(), svelte(), mdx()],
   output: "hybrid",
   adapter: vercel()
