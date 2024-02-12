01926ef652f58b398cacee0e5d6bd00f642bffba
Nathan <ntmactal@gmail.com>
2024-02-11 22:41:06 -0800

    Started a svelte post alternative

---
+++ src/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let text = "Enter text";
+  export let href = "/";
+  export let styling = "text-white bg-honey-flower-800 hover:bg-honey-flower-950 active:bg-purple-darkest";
+  export let target: string | undefined = undefined;
+
+  const classString =
+    "inline-block rounded-md px-2 py-2 text-center uppercase tracking-wide focus:outline-none focus:ring focus:ring-offset-white transition duration-200".concat(
+        " ".concat(styling)
+    );
+</script>
+
+
+<a href={href} class={classString} target={target}>{text}</a>
