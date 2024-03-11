a2f63774139f1bf16f60965d3b28b4b6d52603b8
Nathan Mactal <nathan.mactal@intellimind.com>
2024-03-10 21:22:20 -0700

    moved tags file

---
+++ src/components/Posts/tags.tsx
@@ -0,0 +1,24 @@
+export const generalTags = [
+  "Victories",
+  "Political Action",
+  "Events",
+  "Member Strength",
+  "Safe Staffing",
+  "Solidarity",
+];
+
+export const shopTags = [
+  "HCA",
+  "Sunrise",
+  "Southern Hills",
+  "Mountain View",
+  "Dignity St. Rose",
+  "LVCVA",
+  "Clark County",
+  "UMC",
+  "SNRHA",
+  "RTC",
+  "Renown",
+  "Home Care",
+  "SNHD",
+];
