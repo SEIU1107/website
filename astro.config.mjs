5f6a6c5b9693cbb90895eadcd0a1115360c137b6
Nathan <ntmactal@gmail.com>
2024-08-28 21:59:02 -0700

    converted to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   outDir: "../html",
   redirects: {
     '/communities_united': '/communities_united_2023'
