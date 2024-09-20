9b55e81cc4b5631c2b410863c87debe73f6e988e
Nathan <ntmactal@gmail.com>
2024-09-19 23:04:00 -0700

    endorsements

---
+++ src/components/BallotReady.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Subtitle from "./Subtitle.svelte";
+</script>
+
+<Subtitle text="Find Your Voting Location!" />
+<div
+  class="civicengine-address"
+  data-input-email="required"
+  data-input-first-name="required"
+  data-input-last-name="required"
+  data-input-notifications-opt-in="optional"
+  data-input-phone="optional"
+></div>
---
+++ src/components/Subtitle.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let text = "hello";
+  export let styling =
+    "pb-2 text-center text-honey-flower-800 italic font-Trebuchet";
+</script>
+
+<h1
+  class={styling.concat(
+    " ",
+    text.length <= 50 ? "text-2xl lg:text-3xl" : "text-xl"
+  )}
+>
+  {text}
+</h1>
