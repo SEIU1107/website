cf7bb4429f3e11154479daa3b5fa1971713c827e
Nathan <ntmactal@gmail.com>
2024-12-16 13:57:26 -0800

    updated to astro 5

---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import HeadingTitle from "../components/HeadingTitle.svelte";
+</script>
+
+<div class="flex flex-col justify-center m-auto max-w-screen-xl py-2">
+  <HeadingTitle text="Contact Us" />
+</div>
---
+++ src/components/Forms/TextInput.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let {
+    // When true, the placeholder is the initial value, otherwise the initial value is ''
+    usePlaceholderAsDefault = false,
+
+    // Placeholder to put in the text box
+    placeholder = "",
+    maxlength = 20,
+    styling = "max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+    update, // Handles the current value of the input
+  } = $props();
+
+  let value = $state("");
+
+  onMount(() => {
+    update(usePlaceholderAsDefault ? placeholder : "");
+  });
+</script>
+
+<input
+  oninput={() => {
+    update(value);
+  }}
+  bind:value
+  {maxlength}
+  {placeholder}
+  type="text"
+  id="default-input"
+  class={styling}
+/>
