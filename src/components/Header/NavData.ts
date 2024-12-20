2c05f16c5a345c659b1af2032637c205347596f4
Nathan <nmactal@seiunv.org>
2024-12-20 00:14:04 -0800

    removed membership trainings

--- src/components/Header/NavData.ts
+++ src/components/Header/NavData.ts
@@ -48,10 +48,6 @@ const getInvolved = [
     href: "/events",
   },
   {
-    pageName: "Membership Trainings",
-    href: "/membership_trainings",
-  },
-  {
     pageName: "Political Action",
     href: "/political_action",
   },
